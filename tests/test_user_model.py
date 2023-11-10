db444d75e9dd9e8a7ebefb29193ab06ab4ddf85b
arpit8423 <60109509+arpit8423@users.noreply.github.com>
2023-11-10 11:01:53 +0530

    Update test_user_model.py

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -19,7 +19,7 @@ class UserModelTestCase(unittest.TestCase):
         self.app_context.pop()
 
     def test_password_setter(self):
-        u = User(password='dog')
+        u = User(password='cat')
         self.assertTrue(u.password_hash is not None)
 
     def test_no_password_getter(self):
@@ -29,7 +29,7 @@ class UserModelTestCase(unittest.TestCase):
 
     def test_password_verification(self):
         u = User(password='cat')
-        self.assertTrue(u.verify_password('cat'))
+        self.assertTrue(u.verify_password('dog'))
         self.assertFalse(u.verify_password('dog'))
 
     def test_password_salts_are_random(self):
